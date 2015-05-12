2b11a020dbe3a36f4a435dc3ce06fd0811636fc9
trijeetm <trijeetm@gmail.com>
2015-05-12 14:51:09 -0700

    Added API endpoints for messaging a list

--- app/server/routes/index.js
+++ app/server/routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
  
-router.post('/send_message', function(req, res) {
+router.post('/message_number', function(req, res) {
   var resp = {};
   var msg = req.body;
  
@@ -11,13 +11,37 @@ router.post('/send_message', function(req, res) {
     res.json(resp);
   }
  
-  var twilioClient = require('../twilio/message').sendMsg(msg.to, msg.text, function(error, message) {
+  var twilioClient = require('../twilio/message').messageNumber(msg.to, msg.text, function(error, message) {
     if (error) {
       resp.status = "error";
       resp.message = error;
     } else {
       resp.status = "success";
       resp.message = message.sid;
+    }
+ 
+    res.json(resp);
+  });
+ 
+});
+ 
+router.post('/message_list', function(req, res) {
+  var resp = {};
+  var msg = req.body;
+ 
+  if (!msg || !msg.listId || !msg.text) {
+    resp.status = "error";
+    resp.message = "invalid data";
+    res.json(resp);
+  }
+ 
+  var twilioClient = require('../twilio/message').messageList(msg.listId, msg.text, function(error, message) {
+    if (error) {
+      resp.status = "error";
+      resp.message = error;
+    } else {
+      resp.status = "success";
+      resp.message = message;
     }
  
     res.json(resp);
--- app/server/twilio/message.js
+++ app/server/twilio/message.js
@@ -3,10 +3,12 @@ var authToken = '4182dfa563d7e71f3cc8c3ec08149d47';
 
 var client = require('twilio')(accountSid, authToken);
 
+var fs = require('fs');
+
 var logger = require('../logger/log');
 var msg = {};
  
-msg.sendMsg = function(to, message, callback) {
+msg.messageNumber = function(to, message, callback) {
   client.sendMessage({
     to: to,
     from: "+14154888950", 
@@ -24,8 +26,45 @@ msg.sendMsg = function(to, message, callback) {
         "message": message
       });
     }
+
     callback(error, message);
   });
+};
+ 
+msg.messageList = function(listId, message, callback) {
+  // ignoring listId for now
+  listId = 1;
+
+  // open csv file
+  var numbersCSVFile = "data/lists/safenumbers.csv";
+  var numbersCSV = fs.readFileSync(numbersCSVFile);
+  var numbers = numbersCSV.toString().split(new RegExp('\r|\n', 'g'));
+
+  for (var i = 0; i < numbers.length; i++) {  
+    var number = numbers[i];
+    client.sendMessage({
+      to: number,
+      from: "+14154888950", 
+      body: message 
+    }, function(error, message) {
+      // Log the response to DiskDB for auditing purposes
+      if (error) {
+        logger.logMsg({
+          "status": "error",
+          "error": error
+        });
+      } else {
+        logger.logMsg({
+          "status": "success",
+          "message": message
+        });
+      }
+    });
+  }
+  
+  var _error;
+  var _message = listId + ": " + message;
+  callback(_error, _message);
 };
  
 module.exports = msg;
